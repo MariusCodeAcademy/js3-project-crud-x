24fd191ff72ae160a3629fa581eb2fb8f24b56e5
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-07 08:22:59 +0300

    round total, min qty 0

--- src/components/cart/cartItem.jsx
+++ src/components/cart/cartItem.jsx
@@ -14,7 +14,7 @@ class CartItem extends Component {
   }
 
   handleQty = ({ target }) => {
-    this.setState({ qty: target.value });
+    target.value >= 0 && this.setState({ qty: target.value });
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -52,11 +52,12 @@ class CartItem extends Component {
         </div>
         <div className="cart-col">
           <h3 className="d-upto-800">Price</h3>
-          <h3 className="price">{price} eur</h3>
+          <h3 className="price">{+price.toFixed(2)} eur</h3>
         </div>
         <div className="cart-col">
           <h3 className="d-upto-800">Quantity</h3>
           <input
+            min="0"
             className="cart-qty"
             type="number"
             value={this.state.qty}
@@ -65,7 +66,7 @@ class CartItem extends Component {
         </div>
         <div className="cart-col">
           <h3 className="d-upto-800">Total</h3>
-          <h3 className="price-total">{this.state.total} eur</h3>
+          <h3 className="price-total">{+this.state.total.toFixed(2)} eur</h3>
         </div>
       </div>
     );
