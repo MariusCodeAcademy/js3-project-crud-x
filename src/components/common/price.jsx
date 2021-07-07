9826fb31f6ea20a916c6f01b655d7b3a5e11e9a2
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-07 10:07:22 +0300

    module css for price, conditional classes

--- src/components/common/price.jsx
+++
@@ -1,9 +0,0 @@
-const Price = (props) => {
-  return (
-    <p className="price">
-      {props.children} eur sale price: {props.salePrice}
-    </p>
-  );
-};
-
-export default Price;
---
+++ src/components/common/price/price.jsx
@@ -0,0 +1,16 @@
+import css from './price.module.css';
+const Price = (props) => {
+  const { salePrice, children } = props;
+  return (
+    <p className="">
+      <span className={salePrice ? css.salePrice : ''}>{salePrice}</span>{' '}
+      <span className={salePrice ? css.price : ''}>{children}</span> eur
+    </p>
+  );
+};
+
+function formatPrice(price) {
+  return price + ' eur';
+}
+
+export default Price;
