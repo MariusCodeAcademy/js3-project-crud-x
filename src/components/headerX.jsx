b853f027bbe3ebf35fd6c37291cf67431347a1eb
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-13 16:12:13 +0300

    animation works

--- src/components/headerX.jsx
+++ src/components/headerX.jsx
@@ -6,11 +6,21 @@ class HeaderX extends Component {
     super(props);
     this.state = {
       showNav: false,
+      menuHeight: '0px',
     };
+    // sukuriam nuoroda i el pan kaip su getElbyId
+    this.mainNavRef = React.createRef();
   }
-  // padaryti kad paspaudus nat burgerio iskvvieciam funkcija kuri kazka islogina
 
-  // vietoj logini funkcija keicia showNav savybe i priesinga
+  // padaryti kad paspaudus nat burgerio iskvvieciam funkcija kuri kazka islogina
+  toggleBurger = () => {
+    // vietoj logini funkcija keicia showNav savybe i priesinga
+    const currentStateNav = this.state.showNav;
+    this.setState({
+      showNav: !currentStateNav,
+      menuHeight: !this.state.showNav ? this.mainNavRef.current.scrollHeight : 0,
+    });
+  };
 
   // kai showNav === true mes prideam open clase prie main-nav
 
@@ -21,14 +31,18 @@ class HeaderX extends Component {
           <Link to="/" className="logo">
             JS3<strong>Shop</strong>X
           </Link>
-          <nav className="main-nav open">
+          <nav
+            style={{ maxHeight: this.state.menuHeight }}
+            ref={this.mainNavRef}
+            className={`main-nav ${this.state.showNav ? 'open' : ''} `}
+          >
             {this.props.navLinks.map((item) => (
               <Link key={item.title} className="nav-link-item" to={item.to}>
                 {item.title}
               </Link>
             ))}
           </nav>
-          <button className="mobile-burger">
+          <button onClick={this.toggleBurger} className="mobile-burger">
             <i className="mobile-burger__icon  fa fa-bars"></i>
           </button>
           {this.props.currentUser._id && (
