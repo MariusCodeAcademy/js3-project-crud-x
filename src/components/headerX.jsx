53e2a0b9bd850e2b1df86070da85eb121dbc398d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-13 14:39:44 +0300

    ready for burger to work

--- src/components/headerX.jsx
+++ src/components/headerX.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Button from './common/button/button';
+
 class HeaderX extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showNav: false,
+    };
+  }
+  // padaryti kad paspaudus nat burgerio iskvvieciam funkcija kuri kazka islogina
+
+  // vietoj logini funkcija keicia showNav savybe i priesinga
+
+  // kai showNav === true mes prideam open clase prie main-nav
+
   render() {
     return (
       <header className="header mb-1">
@@ -9,7 +21,7 @@ class HeaderX extends Component {
           <Link to="/" className="logo">
             JS3<strong>Shop</strong>X
           </Link>
-          <nav className="main-nav">
+          <nav className="main-nav open">
             {this.props.navLinks.map((item) => (
               <Link key={item.title} className="nav-link-item" to={item.to}>
                 {item.title}
