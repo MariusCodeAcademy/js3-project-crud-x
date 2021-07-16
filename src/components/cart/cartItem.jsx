f09007658600b2440009f05ef64e372a0fdeac9a
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-07-16 10:02:34 +0300

    fixMaxItemStock in progress

--- src/components/cart/cartItem.jsx
+++ src/components/cart/cartItem.jsx
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getSingleItem } from '../../utils/requests';
 import Button from '../common/button/button';
 
 class CartItem extends Component {
   state = {
-    qty: 1,
+    qty: 0,
     image: '',
-    total: 0,
+    total: null,
+    maxItemInStock: null,
   };
+
+  /** gauti individualu kiek itemu ir irasyti ji i maxItemInStock
+   * @returns {Number} items left in stock
+   */
+  async getCurrentWarehouseStock() {
+    // pasinaudoti getSingleItem funkcija ir gauti item kieki
+    const shopItem = await getSingleItem(this.props.item.itemId);
+    // console.log('shopItem', shopItem.quantity);
+    return shopItem.quantity;
+  }
 
   getTotal() {
     return this.state.qty * this.props.item.price;
   }
 
+  /**
+   * Do not alow to add to stock more than maxItemInStock + qty
+   */
+  fixMaxItemStock(newCartQuantity) {
+    const { qty: itemsInCart, maxItemInStock } = this.state;
+    // what is total item qty
+    const itemQtyCartAndWarehose = +itemsInCart + maxItemInStock;
+    // if we try to set it to more return max
+    if (newCartQuantity > itemQtyCartAndWarehose) {
+      // jei bandom ivesti daugiau nei turim krepselyje ir liko sandelyje tai grazinam maximalu kieki
+      return itemQtyCartAndWarehose;
+    }
+    return newCartQuantity;
+  }
+
   handleQty = ({ target }) => {
-    target.value >= 0 && this.setState({ qty: target.value });
+    if (target.value <= 0) return;
+    this.setState({ qty: this.fixMaxItemStock(target.value) });
+
+    // jei bandom ivesti didesne reiksme nei liko stocke(maxItemInStock) tai norim palikti
+    //maximalia reiksme
+    // ir pranesti su toasty kad pasiem max item limit
+
     // cia iskviesti updateQuantity ir paduoti id ir nauja value
     this.props.onQuantity(this.props.item._id, target.value);
   };
@@ -23,16 +56,29 @@ class CartItem extends Component {
     if (prevState.qty !== this.state.qty) {
       this.setState({ total: this.getTotal() });
     }
+    // call fixMaxItemStock
   }
 
   componentDidMount() {
-    const { image, quantity } = this.props.item;
-    // dynamic import
-    // import(`../../static/shop/${image}1.jpg`).then((img) => {
-    //   console.log(img.default);
-    // });
-    const imgImported = require(`../../static/shop/${image}1.jpg`).default;
-    this.setState({ qty: quantity, image: imgImported, total: this.getTotal() });
+    // IFFE - imediately invoked function expresion
+    (async () => {
+      const warehouseQty = await this.getCurrentWarehouseStock();
+      console.log({ warehouseQty });
+
+      const { image, quantity } = this.props.item;
+      // dynamic import
+      // import(`../../static/shop/${image}1.jpg`).then((img) => {
+      // import { getSingleItem } from '../../utils/requests';
+
+      // });
+      const imgImported = require(`../../static/shop/${image}1.jpg`).default;
+      await this.setState({
+        qty: quantity,
+        image: imgImported,
+        total: this.getTotal(),
+        maxItemInStock: warehouseQty,
+      });
+    })();
   }
   render() {
     const { price, title, color, size } = this.props.item;
@@ -68,7 +114,7 @@ class CartItem extends Component {
         </div>
         <div className="cart-col">
           <h3 className="d-upto-800">Total</h3>
-          <h3 className="price-total">{+this.state.total.toFixed(2)} eur</h3>
+          <h3 className="price-total">{this.state.total && +this.state.total.toFixed(2)} eur</h3>
         </div>
       </div>
     );
